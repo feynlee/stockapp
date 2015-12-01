d217354e9e2c95f6870374e4b3948c2307d0213b
Ziyue Li <liziyue241@gmail.com>
2015-12-01 10:46:38 -0500

    fixed selection bug, changed legend position to top left

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from bokeh.plotting import figure
 from bokeh.embed import components
-from bokeh.palettes import Spectral11
-import requests
-import simplejson as json
-import pandas as pd
+# import simplejson as json
+# import pandas as pd
 import Quandl
 
 app = Flask(__name__)
@@ -22,17 +20,13 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     else:
-        r=request.form
-        # app.vars['ticker']=request.form['ticker_name']
-        stock=request.form['ticker_name']
-
-        options=['Close','Adj. Close','Open','Adj. Open']
+        r = request.form
+        stock = request.form['ticker_name']
+        options = ['Close','Adj. Close','Open','Adj. Open']
+        selectoptions=[]
         for item in options:
-            if item in request.form:
-                continue
-            else:
-                options.remove(item)
-
+            if item in r:
+                selectoptions.append(item)
 
 
         api_key = 'yhxmmVTWyYhsYwjwQdjx'
@@ -44,42 +38,17 @@ def index():
               x_axis_label='date',
               x_axis_type='datetime')
 
-        # numlines = len(options)
-        # colors=[]
-        options=['Open','Close','Adj. Open','Adj. Close']
         colorselect = dict([('Close','blue'),('Adj. Close','green'),('Open','orange'),('Adj. Open','red')])
-        # for item in options:
-        #     colors.append(colorselect[item])
-        #
-        for item in options:
-            plot.line(x=mydata.index.tolist(), y=mydata[item], line_color = colorselect[item], legend = item)
+        for item in selectoptions:
+            plot.line(x=mydata.index.tolist(), y=mydata[item], line_color = colorselect[item], legend = stock+": "+item)
+        plot.legend.orientation = "top_left"
 
-        # item='Open'
-        # plot.line(x=mydata.index.tolist(), y=mydata[item],line_color='red')
-        print(request.form)
-        print(item)
-
-
-        # plot.line(pd_combined['Date'].tolist(), pd_combined['Open'].tolist(), line_color='red')
-        # plot.multi_line(xs=[[1,2,3]], ys=[[3,4,8]],color=['red'])
-        # plot.line([1,2,3], [3,4,8],line_color=['red'])
-
-
-
-        # app.vars['Open']=request.form['Open']
-        # app.vars['Close']=request.form['Close']
-        # app.vars['Adj_Open']=request.form['Adj. Open']
-        # app.vars['Adj_Close']=request.form['Adj. Close']
-
-        # print(json_object)
-
-
-        # f = open('data.txt','w')
-        # f.write('tiker: %s\n%s\n%s\n%s\n%s\n'%(app.vars['ticker'],app.vars['Open'],app.vars['Close'],app.vars['Adj_Open'],app.vars['Adj_Close']))
-        # f.close()
+        # print(r)
+        # print(selectoptions)
 
         script, div = components(plot)
-        return render_template('graph.html', script=script, div=div)
+        return render_template('graph.html', script=script, div=div, stock=stock)
+
         # return 'request.method was not a GET!'
 
 # @app.route('/graph',methods=['GET','POST'])
@@ -92,5 +61,5 @@ def index():
 #         f.write('tiker: %s/n'%app.vars['ticker'])
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-  app.run(port=33507)
+    app.run(debug=True)
+    # app.run(port=33507)
