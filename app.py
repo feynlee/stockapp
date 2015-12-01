c7c4b913da2c1673f2670fe34d2a1b8c250778cd
Ziyue Li <liziyue241@gmail.com>
2015-12-01 11:30:43 -0500

    fixed minor bugs and pep8ed the file

--- app.py
+++ app.py
@@ -7,47 +7,56 @@ import Quandl
 
 app = Flask(__name__)
 
-select={}
-
+select = {}
 
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
-@app.route('/index',methods=['GET','POST'])
+
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
     else:
         r = request.form
         stock = request.form['ticker_name']
-        options = ['Close','Adj. Close','Open','Adj. Open']
-        selectoptions=[]
+        options = ['Close', 'Adj. Close', 'Open', 'Adj. Open']
+        selectoptions = []
         for item in options:
             if item in r:
                 selectoptions.append(item)
 
-
         api_key = 'yhxmmVTWyYhsYwjwQdjx'
-        mydata = Quandl.get('WIKI/'+stock, authtoken= api_key)
+        mydata = Quandl.get('WIKI/'+stock, authtoken=api_key)
 
         plot = figure(
-              # tools=TOOLS,
-              title='Data from Quandle WIKI set',
-              x_axis_label='date',
-              x_axis_type='datetime')
+            # tools=TOOLS,
+            title='Data from Quandle WIKI set',
+            x_axis_label='date',
+            x_axis_type='datetime')
 
-        colorselect = dict([('Close','blue'),('Adj. Close','green'),('Open','orange'),('Adj. Open','red')])
+        colorselect = dict(
+            [('Close', 'blue'), ('Adj. Close', 'green'), ('Open', 'orange'),
+             ('Adj. Open', 'red')])
         for item in selectoptions:
-            plot.line(x=mydata.index.tolist(), y=mydata[item], line_color = colorselect[item], legend = stock+": "+item)
+            plot.line(
+                x=mydata.index.tolist(),
+                y=mydata[item],
+                line_color=colorselect[item],
+                legend=stock+": "+item)
         plot.legend.orientation = "top_left"
 
         # print(r)
         # print(selectoptions)
 
         script, div = components(plot)
-        return render_template('graph.html', script=script, div=div, stock=stock)
+        return render_template(
+            'graph.html',
+            script=script,
+            div=div,
+            stock=stock)
 
         # return 'request.method was not a GET!'
 
